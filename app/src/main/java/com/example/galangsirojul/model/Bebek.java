2fea12b33fb70daf1280a3ce99f16301129502cf
Glgproject <glgmarvelous2002@gmail.com>
2021-12-07 13:38:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/galangsirojul/model/Bebek.java
@@ -0,0 +1,11 @@
+package com.example.galangsirojul.model;
+
+public class Bebek extends Hewan{
+
+    public Bebek(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Bebek",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
+
